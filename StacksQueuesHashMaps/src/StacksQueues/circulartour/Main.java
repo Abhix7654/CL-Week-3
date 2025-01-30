23c5ce1d0d5075edb95bc7af4ccd4b7d42b66a0f
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-01-30 14:39:05 +0530

    Problem 5 solution queue

--- StacksQueuesHashMaps/src/StacksQueues/circulartour/Main.java
+++ StacksQueuesHashMaps/src/StacksQueues/circulartour/Main.java
@@ -3,10 +3,10 @@ package StacksQueues.circulartour;
 public class Main {
     public static void main(String[] args) {
         PetrolPump[] pumps = {
-                new PetrolPump(4, 6),
-                new PetrolPump(6, 5),
-                new PetrolPump(7, 3),
-                new PetrolPump(4, 5)
+                new PetrolPump(9, 6),
+                new PetrolPump(6, 1),
+                new PetrolPump(5, 3),
+                new PetrolPump(2, 4)
         };
 
         int startIndex = CircularTour.findStartingPoint(pumps);
